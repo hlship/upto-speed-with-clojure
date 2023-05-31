6acb60e6dfba057903a20ae6ddbce53c4ca25d47
Howard M. Lewis Ship <hlship@gmail.com>
2023-05-31 12:30:12 -0700

    Work on the set of 9 Star Wars people documents

--- src/net/lewisship/star_wars.clj
+++ src/net/lewisship/star_wars.clj
@@ -4,8 +4,12 @@
             [clojure.string :as string]
             [nextjournal.clerk :as clerk]))
 
-(def people-raw (json/read (io/reader (io/resource "data/star-wars/people-1.json"))
-                           :key-fn keyword))
+(def people-docs (apply conj []
+                        (for [i (range 1 10)
+                              :let [path   (str "data/star-wars/people-" i ".json")
+                                    url    (io/resource path)
+                                    reader (io/reader url)]]
+                          (json/read reader :key-fn keyword))))
 
 (defn- set-from-comma-separated
   [s]
@@ -16,78 +20,118 @@
   "Filter and rename keys, convert values to be more natural in Clojure."
   [person]
   {:birth-year (:birth_year person)
-   :height     (Long/parseLong (:height person))
+   :height     (parse-long (:height person))
    :gender     (keyword (:gender person))
    :name       (:name person)
    :films      (:films person)
    :eye-color  (keyword (:eye_color person))
-   :mass       (Long/parseLong (:mass person))
+   :mass       (parse-long (:mass person))
    :hair-color (set-from-comma-separated (:hair_color person))
    :skin-color (set-from-comma-separated (:skin_color person))})
 
 
-(for [person (:results people-raw)]
+(for [doc    people-docs
+      person (:results doc)]
   (simplify-person person))
 
-(let [raw-persons (:results people-raw)
+
+(apply concat (for [doc people-docs]
+                (map simplify-person (:results doc))))
+
+(let [raw-persons (mapcat :results people-docs)
       persons     (map simplify-person raw-persons)
-      just-mass   (map :mass persons)
+      just-mass   (keep :mass persons)
       total-mass  (apply + just-mass)]
   total-mass)
 
-(defn total-mass
-  []
+(let [persons (apply concat
+                     (for [doc people-docs]
+                       (map simplify-person (:results doc))))]
   (loop [result 0
-         coll   (map simplify-person (:results people-raw))]
+         coll   persons]
     (if (some? coll)
-      (recur (+ result (:mass (first coll)))
+      (recur (+ result (or (:mass (first coll)) 0))
              (next coll))
       result)))
 
-(total-mass)
 
-(def people (->> people-raw
-                 :results
+(let [raw-persons (mapcat :results people-docs)
+      persons     (map simplify-person raw-persons)]
+  (loop [result 0
+         coll   persons]
+    (if (some? coll)
+      (recur (+ result (or (:mass (first coll)) 0))
+             (next coll))
+      result)))
+
+
+(def people (->> people-docs
+                 (mapcat :results)
                  (map simplify-person)))
 
-(filter #(= :blue (:eye-color %)) people)
+(let [raw-persons (mapcat :results people-docs)
+      persons     (map simplify-person raw-persons)
+      just-mass   (keep :mass persons)
+      total-mass  (apply + just-mass)]
+  total-mass)
+
+(->> (filter #(= :blue (:eye-color %)) people)
+     (map :name))
 
 (->> people
-     (map (fn [person]
-            (let [name (:name person)]
-              (map (fn [film]
-                     {:name name
-                      :film film})
-                   (:films person)))))
-     (reduce into [])
+     (mapcat (fn [person]
+               (let [name (:name person)]
+                 (map (fn [film]
+                        {:name name
+                         :film film})
+                      (:films person)))))
      (reduce (fn [film->names film+name]
                (update film->names (:film film+name)
                        conj (:name film+name)))
              {}))
 
-(let [person->file+names (fn [person]
+(let [person->film+names (fn [person]
                            (let [name (:name person)]
                              (map (fn [film]
                                     {:name name
                                      :film film})
                                   (:films person))))
-      combine-file+names (fn [film->names film+name]
+      combine-film+names (fn [film->names film+name]
                            (update film->names (:film film+name)
                                    conj (:name film+name)))]
   (->> people
-       (map person->file+names)
+       (map person->film+names)
        (reduce into [])
-       (reduce combine-file+names {})))
+       (reduce combine-film+names {})))
 
-(let [person->file+names (fn [person]
+(let [person->film+names (fn [person]
                            (let [name (:name person)]
                              (map (fn [film]
                                     {:name name
                                      :film film})
                                   (:films person))))
-      combine-file+names (fn [film->names film+name]
+      combine-film+names (fn [film->names film+name]
                            (update film->names (:film film+name)
                                    conj (:name film+name)))]
   (->> people
-       (mapcat person->file+names)
-       (reduce combine-file+names {})))
+       (mapcat person->film+names)
+       (reduce combine-film+names {})))
+
+(let [person->film+names (fn [{:keys [name films]}]
+                           (map (fn [film]
+                                  {:name name
+                                   :film film})
+                                films))
+      combine-film+names (fn [film->names {:keys [film name]}]
+                           (update film->names film conj name))]
+  (->> people
+       (mapcat person->film+names)
+       (reduce combine-film+names {})))
+
+(let [person->film+names (fn [{:keys [name films]}]
+                           (map #(vector name %) films))
+      combine-film+names (fn [film->names [name film]]
+                           (update film->names film conj name))]
+  (->> people
+       (mapcat person->film+names)
+       (reduce combine-film+names {})))
